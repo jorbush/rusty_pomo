15678bec3a2eacb3bd2f2bf8bd2e2da50998681e
Jordi <55429631+jorbush@users.noreply.github.com>
2025-08-08 20:09:49 +0200

    fmt

--- src/notifications.rs
+++ src/notifications.rs
@@ -1,9 +1,9 @@
-use std::path::Path;
-use std::time::Duration;
+use crate::state::{AppState, PhaseKind};
 use notify_rust::Notification;
 #[cfg(target_os = "macos")]
 use notify_rust::set_application;
-use crate::state::{AppState, PhaseKind};
+use std::path::Path;
+use std::time::Duration;
 
 #[allow(unused_variables)] // Because it's only used on macOS
 pub fn maybe_init_macos_bundle(args: &crate::args::Args) {
